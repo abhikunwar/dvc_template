49b3b665de725ea0092e34fd2e921eea45b0ae5f
Abhishek Kumar <kumar.abhishek2701@gmail.com>
2022-01-04 16:11:34 +0530

    requirement update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
-AUTHOR_USER_NAME = "c17hawke"
+REPO_NAME = "dvc_nlp"
+AUTHOR_USER_NAME = "Abhishek"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
 
@@ -13,14 +13,14 @@ LIST_OF_REQUIREMENTS = []
 setup(
     name=SRC_REPO,
     version="0.0.1",
-    author=AUTHOR_USER_NAME,
+    author="Abhishek kumar",
     description="A small package for DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="sunny.c17hawke@gmail.com",
+    url=f"https://github.com/abhikunwar/dvc_template.git",
+    author_email="kumar.abhishek2701@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.6",
-    install_requires=LIST_OF_REQUIREMENTS
-)
+    install_requires=LIST_OF_REQUIREMENTS )
+        
